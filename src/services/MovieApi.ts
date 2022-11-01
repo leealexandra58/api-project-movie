e2e48e6b08260b14b7d276bafcf0b9ce63bcaa95
Erinn East <erinn.east@gmail.com>
2022-11-01 19:39:05 -0400

    Seach Movies

--- src/services/MovieApi.ts
+++ src/services/MovieApi.ts
@@ -7,7 +7,7 @@ import { Movie } from "../models/Movie";
 
 
 export function getMoviesApi() {
-    return axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=94537c011511d5109a7ca20f337286f3'
+    return axios.get('https://api.themoviedb.org/3/search/movie?api_key=94537c011511d5109a7ca20f337286f3'
     
     );
 }
